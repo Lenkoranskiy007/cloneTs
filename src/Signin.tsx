7f86d81d8f7d6f13fd7d2ae01841935976246b41
MACBOOK <macbook@MacBook-Pro-MACBOOK.local>
2021-07-09 11:38:30 +0400

    kkkl

--- src/Signin.tsx
+++ src/Signin.tsx
@@ -14,18 +14,25 @@ const useStyles = makeStyles((theme) => ({
         height: 'calc(100vh - 84px)'
     },
     blueSide: {
-        backgroundColor: '#1DA1F2',
+        backgroundColor: '#71C9F8',
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
         flex: '0 0 50%',
-        overflow: 'hidden'
+        overflow: 'hidden',
+        position: 'relative'
+    },
+    blueSideBigIcon: {
+        position: 'absolute',
+        left: '50%',
+        top: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: '350%',
+        height: '253%'
     },
 
     blueSideListInfo: {
-     
-
-
+      position: 'relative',
       listStyle: 'none',
       padding: 0,
       margin: 0,
@@ -39,8 +46,13 @@ const useStyles = makeStyles((theme) => ({
       }
     },
 
+    blueSideListInfoItem: {
+      marginBottom: 40,
+    },
+
     blueSideListInfoIcon: {
-        fontSize: 32
+        fontSize: 32,
+        marginRight: 15,
     },
 
     loginSide: {
@@ -76,19 +88,18 @@ export const  Signin = () => {
     return (
         <div className={classes.wrapper}>
         <div className={classes.blueSide}>
+        <TwitterIcon color="primary" className={classes.blueSideBigIcon}/>
             <ul className={classes.blueSideListInfo}>
-                <li><Typography variant="h6"><SearchIcon  className={classes.blueSideListInfoIcon}/>Читайте о том , что вам интересно</Typography>
+                <li   className={classes.blueSideListInfoItem}><Typography variant="h6"><SearchIcon  className={classes.blueSideListInfoIcon}/>Читайте о том , что вам интересно.</Typography>
                 </li>
-                <li>
+                <li  className={classes.blueSideListInfoItem}>
                 <Typography variant="h6"> <PeopleOutlineIcon className={classes.blueSideListInfoIcon}/>Узнайте , о чем говорят в мире </Typography>
                 </li>
-                <li>
+                <li  className={classes.blueSideListInfoItem}>
                 <Typography variant="h6"><MessageIcon className={classes.blueSideListInfoIcon}/>Присоединяйтесь к общению</Typography>
                 </li>
             </ul>
-
         </div>
-        
          <div className={classes.loginSide}>
              <div className={classes.loginSideWrapper}>
             <TwitterIcon color="primary" className={classes.loginSideTwitterIcon}/>
